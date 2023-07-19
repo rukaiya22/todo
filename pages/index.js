f20d7eddaa6582777085fa603c6c24b882709f39
rukaiya22 <rukaiyarafique2000@gmail.com>
2023-07-19 18:04:28 +0600

    ABCD

---
+++ pages/index.js
@@ -0,0 +1,48 @@
+export default function name() {
+  return (
+    <div className="container">
+      <h1>Online cat shop</h1>
+      <p>
+        The cat is a domestic species of small carnivorous mammal. It is the
+        only domesticated species in the family Felidae and is commonly referred
+        to as the domestic cat or house cat to distinguish it from the wild
+        members of the family Lorem Ipsum is simply dummy text of the printing
+        and typesetting industry. Lorem Ipsum has been the industry's standard
+        dummy text ever since the 1500s, when an unknown printer took a galley
+        of type and scrambled it to make a type specimen book. It has survived
+        not only five centuries, but also the leap into electronic typesetting,
+        remaining essentially unchanged. It was popularised in the 1960s with
+        the release of Letraset sheets containing Lorem Ipsum passages, and more
+        recently with desktop publishing software like Aldus PageMaker including
+        versions of Lorem Ipsum.
+      </p>
+      <br></br>
+      <div>
+        <form>
+          <label>
+            Enter your name:
+            <input type="text" />
+            <br></br>
+            <br></br>
+            Enter your phone name:
+            <input type="text" />
+            <br></br>
+            <br></br>
+            select your quantity:
+            <input type="number" />
+            <br></br>
+            <br></br>
+            <button type="submit">Submit</button>
+          </label>
+        </form>
+        <div>
+          <img src="img/1.jpg "></img>
+          <p>
+            Your name is Rukaiya Rafique<br></br> your phone number is 017100000
+            <br></br> select quantity 2
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
