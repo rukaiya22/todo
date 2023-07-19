30ba936e2d09bb6b4104d34681c515f32c3ca6fc
anirban00537 <anirban00537@gmail.com>
2023-07-19 18:13:11 +0600

    changes by anirban

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
 export default function name() {
   return (
     <div className="container">
-      <h1>Online cat shop</h1>
+      <h1>Online dog shop</h1>
       <p>
         The cat is a domestic species of small carnivorous mammal. It is the
         only domesticated species in the family Felidae and is commonly referred
         to as the domestic cat or house cat to distinguish it from the wild
-        members of the family Lorem Ipsum is simply dummy text of the printing
-        and typesetting industry. Lorem Ipsum has been the industry's standard
-        dummy text ever since the 1500s, when an unknown printer took a galley
-        of type and scrambled it to make a type specimen book. It has survived
-        not only five centuries, but also the leap into electronic typesetting,
-        remaining essentially unchanged. It was popularised in the 1960s with
-        the release of Letraset sheets containing Lorem Ipsum passages, and more
-        recently with desktop publishing software like Aldus PageMaker including
-        versions of Lorem Ipsum.
       </p>
       <br></br>
       <div>
